6d5deb048a1e1f94e63ff184cbfb1d3b9de66e5e
creator <anthonicito666@gmail.com>
2019-04-27 09:52:46 -0500

    primer Commit

---
+++ suraApp/src/main/java/com/sura/suraApp/SuraAppApplication.java
@@ -0,0 +1,13 @@
+package com.sura.suraApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuraAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuraAppApplication.class, args);
+	}
+
+}
